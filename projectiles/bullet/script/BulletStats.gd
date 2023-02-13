69bf71bda7d42531bbf9914ed32ceda7e7a191b5
goodguyplayer <nathanbritodasilva@gmail.com>
2023-02-13 11:28:39 -0200

    Bullet update - Part 1  - Added script to hitbox

--- projectiles/bullet/script/BulletStats.gd
+++ projectiles/bullet/script/BulletStats.gd
@@ -1,12 +1,14 @@
-extends RigidBody2D
+extends Area2D
 
 export var damage = 1
 export var knockback_vector = Vector2.ZERO
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-	
 
-func _on_Bullet_body_entered(body):
-	queue_free()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
