20efb663f68f1fcc47be60a579ca5085aaca0f69
goodguyplayer <nathanbritodasilva@gmail.com>
2023-02-14 15:56:53 -0200

    Bullets - Swapped body collision to area collision

--- projectiles/bullet/script/Bullet.gd
+++ projectiles/bullet/script/Bullet.gd
@@ -7,7 +7,7 @@ export var knockback_vector = Vector2.ZERO
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-	
 
-func _on_Bullet_body_entered(body):
+
+func _on_BulletHitbox_area_entered(area):
 	queue_free()
