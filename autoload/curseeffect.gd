33d7ee722e9e88afa3c41a6fad86cef60f35e61a
goodguyplayer <nathanbritodasilva@gmail.com>
2023-02-15 10:35:31 -0200

    Attempted to create resource for CurseEffects

---
+++ autoload/curseeffect.gd
@@ -0,0 +1,15 @@
+extends Resource
+class_name CurseEffect
+
+export (String) var CurseName = ""
+export (String) var CurseDesc = ""
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
