1c58849b59d33005284a07a462cd98f5056eed13
goodguyplayer <nathanbritodasilva@gmail.com>
2023-02-16 15:22:30 -0200

    Floor - Export var to see whether collision is enabled or not

--- world/floor/Floor.gd
+++ world/floor/Floor.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 onready var collision_shape_2d = $CollisionShape2D
 
+export var collision_status = false
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,7 +12,9 @@ func _ready():
 
 func change_collision_swap():
 	collision_shape_2d.disabled = not collision_shape_2d.disabled
+	collision_status = collision_shape_2d.disabled 
 	
 	
 func change_collision(value : bool) -> void:
 	collision_shape_2d.disabled = value
+	collision_status = collision_shape_2d.disabled 
