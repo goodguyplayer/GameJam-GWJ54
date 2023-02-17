f6f1cbc5d97fc3ca527512e573c4c7119e6bb5ac
goodguyplayer <nathanbritodasilva@gmail.com>
2023-02-17 00:21:43 -0200

    Created a Pause scene
    To add options when options exist

---
+++ UI/Pause.gd
@@ -0,0 +1,25 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _input(event):
+	if event.is_action_pressed("ui_pause"):
+		var new_pause_state = not get_tree().paused
+		get_tree().paused = new_pause_state
+		visible = new_pause_state
+
+func _on_Resume_pressed():
+	get_tree().paused = false
+	visible = false
+
+
+func _on_ReturnToMenu_pressed():
+	get_tree().change_scene("res://Menus/scenes/MainMenu.tscn")
+
+
+func _on_Options_pressed():
+	pass # Replace with function body.
