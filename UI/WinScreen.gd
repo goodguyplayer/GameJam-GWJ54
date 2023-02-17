58ff536ad2c08d01a5cb6aaf0e2052340d893c39
goodguyplayer <nathanbritodasilva@gmail.com>
2023-02-16 23:19:45 -0200

    Created win screen

---
+++ UI/WinScreen.gd
@@ -0,0 +1,30 @@
+extends Control
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_StartAgain_pressed():
+	get_tree().change_scene("res://world/scenes/WorldDefault.tscn")
+
+
+func _on_TryEndless_pressed():
+#	get_tree().change_scene("res://Menus/scenes/MainMenu.tscn")
+	pass
+
+
+func _on_ReturnMenu_pressed():
+	get_tree().change_scene("res://Menus/scenes/MainMenu.tscn")
+
+
+func _on_Exit_pressed():
+	get_tree().quit()
