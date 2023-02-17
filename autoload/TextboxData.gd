1ba2a156e5ac2e6c5053db264de393b18a69132d
goodguyplayer <nathanbritodasilva@gmail.com>
2023-02-17 19:11:01 -0200

    Autoload - Textboxdata resource script created

---
+++ autoload/TextboxData.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name TextboxData
+
+export (String) var SpeakerTextureLeft = ""
+export (String) var SpeakerTextureRight = ""
+export (String) var title = ""
+export (bool) var SpeakerLeftVisibility = true
+export (bool) var SpeakerRightVisibility = true
+export (Array) var text_queue = []
