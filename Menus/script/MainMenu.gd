a263920ed7817b8a143b1fd9c7a0adcaf72877c8
goodguyplayer <nathanbritodasilva@gmail.com>
2023-02-19 16:23:33 -0200

    Credit scenes created

--- Menus/script/MainMenu.gd
+++ Menus/script/MainMenu.gd
@@ -34,7 +34,8 @@ func _on_Options_pressed():
 
 
 func _on_Credits_pressed():
-	pass # Replace with function body.
+	var credits = load("res://Menus/scenes/Credits.tscn").instance()
+	get_tree().current_scene.add_child(credits)
 
 
 func _on_Exit_pressed():
